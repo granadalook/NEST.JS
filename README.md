c58cb65f8bd8d266eaed4f12ae63c691740fdc33
granadalook <tiven17.jsga@gmail.com>
2022-09-01 10:36:20 -0500

    CLASE 10 :   Se crean  los controladores  en  carpepas para unica responsabilidad

--- README.md
+++ README.md
@@ -1,21 +1,31 @@
 # NEST.JS
 
+APP OFICIAL https://nestjs.com/
+
 Nest.js CRUD API
 
 # INSTALACION DE NEST GLOBAL
-
 1: TENER NODE INSTALADO
 2: COMANDO --- npm i -g @nestjs/cli
 3: VERSION --- nest --version
 4: AYUDA --- nest --help
 5: IFORMACION --- nest info
 
-# CREAR NUEVO PROYECTO 
-
-1: COMANDO ---  nest  new  nombreProyecto 
+# CREAR NUEVO PROYECTO
+1: COMANDO --- nest new nombreProyecto
 2: MANEJADOR DE PAQUETES --- npm --- preferiblemente
 
-# CORRER  EL PROYECTO 
+# CORRER EL PROYECTO
 
 1: COMANDO --- npm run start
-2: VER ---  http://localhost:3000/
+2: VER --- http://localhost:3000/
+3: COMANDO DESARROLLO --- npm run start:dev
+
+# DECORADORES
+
+HTTP-- @Get()
+PARAMETROS--- @Param()
+QUERY PARAMS --- @Query()
+
+# COMANDOS
+1:CREAR CONTROLLER --- nest g co NOMBRECONTROLADOR
--- api_crud_nest/src/app.controller.ts
+++ api_crud_nest/src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+
 import { AppService } from './app.service';
 
-@Controller()
+@Controller() //decorador
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // decorador http
   getHello(): string {
-    return this.appService.getHello();
+    return 'como jue';
+  }
+  //las rutas no dinamicas van primero en la estructura que las dinamicas
+  @Get('numero/:paramsId')
+  getNumeros(@Param('paramsId') paramsId: number) {
+    return `numeros ${paramsId}`;
+  }
+
+  @Get('numero/:paramsId/numero/:idNumero')
+  getCategorias(
+    @Param('paramsId') paramsId: number,
+    @Param('idNumero') idNumero: number,
+  ) {
+    return `numeros ${paramsId} y ${idNumero}`;
+  }
+
+  //http://localhost:3000/query?limit=50&offset=30&brand=stiven
+  @Get('query')
+  getQuerys(
+    @Query('limit') limit = 150,
+    @Query('offset') offset: number,
+    @Query('brand') brand: string,
+  ) {
+    return `limit ${limit} y tambien offset ${offset} y la marca ${brand} `;
   }
 }
